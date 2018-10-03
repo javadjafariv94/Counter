8812ea109f94661f2bd1053ee02263e958e1c262
javad jafari <javadjafariv93@gmail.com>
2018-10-03 19:54:45 +0330

    first counter

---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {count :0}
+
+    increment = () => {
+       this.setState({
+          count : this.state.count + 1
+       })
+    }
+    decrement = () => {
+      this.setState({
+      count :this.state.count - 1
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <div>
+             <button onClick={this.increment}> + </button>
+             <span>{this.state.count}</span>
+             <button onClick={this.decrement}> - </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
